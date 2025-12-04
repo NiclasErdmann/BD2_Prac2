1b4fd19834a2a3abdd08dd25016741e09b84c8a6
NiclasErdmann <niclas2702@gmail.com>
2025-12-04 20:30:15 +0100

    idk

--- asignarPermisos.php
+++ asignarPermisos.php
@@ -1,28 +1,17 @@
 <?php
 // 2025111303principal.php?usuari=Joan
 $user= $_GET["user"];
-$pasword= $_GET["password"];
+$role= $_GET["role"];
 // preparar select per descarregar tots els privilegis de l usuari
 $con = mysqli_connect("localhost","root","");
 $db = mysqli_select_db($con,"db2_prac2");
-
-/*
-$consulta= "select titol,enlace 
-                from usuari 
-                join rol on usuari.esunrol = rol.rol
-                join potfer on potfer.rol=rol.rol
-                join privilegis on privilegis.idprivilegi = potfer.privilegi
-where usuari.username='".$user."'";
-*/
-
-
 
 $consulta=" select funciones.nombre as nombre
                 from voluntario v
                 join rol on v.idRol = rol.idRol
                 join PuedeHacer on PuedeHacer.idRol=rol.idRol
                 join funciones on funciones.idFunciones = PuedeHacer.idFunciones
-            where v.usuario='".$user."' AND v.contraseña='".$password."'";
+            where v.usuario='".$user."' AND v.contraseña='".$contraseña."'";
 
 $resultat = mysqli_query($con, $consulta);
 
